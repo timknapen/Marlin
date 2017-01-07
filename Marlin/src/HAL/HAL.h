33bc8830620457387704392cb9c93d679f32ce55
teemuatlut <teemuatlut@github.com>
2017-01-07 19:25:05 +0200

    Add HAL file include for Teensy35

--- Marlin/src/HAL/HAL.h
+++ Marlin/src/HAL/HAL.h
@@ -85,6 +85,10 @@ void spiSendBlock(uint8_t token, const uint8_t* buf);
   #define CPU_32_BIT
   #include "HAL_DUE/HAL_Due.h"
   #include "math_32bit.h"
+#elif defined(__MK64FX512__)
+  #define CPU_32_BIT
+  #include "HAL_TEENSY35/HAL_Teensy.h"
+  #include "math_32bit.h"
 #else
   #error Unsupported Platform!
 #endif
